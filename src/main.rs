148542bbe538f815fc573d970cadebf278b73881
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-18 01:43:35 +0200

    Created TODOs list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// TODO: Choose => Option 1: CLI App, Option 2: Terminal App with simple GUI
+// TODO: Search for needed dependencies and include them in cargo.toml
+// TODO: Implement showing files down the specified path
+// TODO: Implement choice of files from printed list
+// TODO: Implement file content encryption
+// TODO: Implement file content decryption
+// TODO: Create option to choose encryption method
+// TODO: Store file encryption method, different custom file extensions?
+
 fn main() {
     println!("Hello, world!");
 }
