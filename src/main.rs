bb8e1a4ea0d9c95fb4899163efc9941281408adf
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-18 02:16:08 +0200

    Add TODO(#6)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // TODO(#3): Implement showing files down the specified path
 // TODO(#4): Implement choice of files from printed list
 // TODO(#5): Implement file content encryption
-// TODO: Implement file content decryption
+// TODO(#6): Implement file content decryption
 // TODO: Create option to choose encryption method
 // TODO: Store file encryption method, different custom file extensions?
 
