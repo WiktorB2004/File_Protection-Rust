1214db70e0cadd46319fa58b47590e7b1d3ce0ad
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-18 02:16:06 +0200

    Add TODO(#4)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // TODO(#1): Choose => Option 1: CLI App, Option 2: Terminal App with simple GUI
 // TODO(#2): Search for needed dependencies and include them in cargo.toml
 // TODO(#3): Implement showing files down the specified path
-// TODO: Implement choice of files from printed list
+// TODO(#4): Implement choice of files from printed list
 // TODO: Implement file content encryption
 // TODO: Implement file content decryption
 // TODO: Create option to choose encryption method
