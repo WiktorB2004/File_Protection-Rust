e4eea97d903b2d6b5b8a289c02b21a53d4631109
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-18 02:16:07 +0200

    Add TODO(#5)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // TODO(#2): Search for needed dependencies and include them in cargo.toml
 // TODO(#3): Implement showing files down the specified path
 // TODO(#4): Implement choice of files from printed list
-// TODO: Implement file content encryption
+// TODO(#5): Implement file content encryption
 // TODO: Implement file content decryption
 // TODO: Create option to choose encryption method
 // TODO: Store file encryption method, different custom file extensions?
