b0faee3fbe7642e9d4defe5723000abf9e359360
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-19 11:18:07 +0200

    Removed done todos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-// TODO(#1): Choose => Option 1: CLI App, Option 2: Terminal App with simple GUI
-// TODO(#2): Search for needed dependencies and include them in cargo.toml
-// TODO(#3): Implement showing files down the specified path
-// TODO(#4): Implement choice of files from printed list
+// TODO(#4): Implement navigation and choice of files from printed list
 // TODO(#5): Implement file content encryption
 // TODO(#6): Implement file content decryption
 // TODO(#7): Create option to choose encryption method
